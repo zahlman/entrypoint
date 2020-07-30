e2a97e576d7b75807ff82a977ed2c4d45f19536a
Karl Knechtel <karl.a.knechtel@gmail.com>
2020-07-30 15:53:12 -0400

    Basic implementation

--- entrypoint/__init__.py
+++ entrypoint/__init__.py
@@ -1,1 +1,60 @@
+from argparse import ArgumentParser
+from functools import partial
+
+
 __version__ = '0.1.0'
+
+
+def _invoke_wrapper(invoke, func, parser, command_line=None):
+    return invoke(func, vars(parser.parse_args(command_line)))
+
+
+def _add_argument(parser, name, spec):
+    if isinstance(spec, str):
+        parser.add_argument(name, help=spec)
+        return
+    if not isinstance(spec, dict):
+        raise TypeError(
+            f'spec for parameter `{name}` must be either string or dict'
+        )
+    if spec.get('keyword', False):
+        del spec['keyword']
+        names = [f'-{name[0]}', f'--{name.replace("_", "-")}']
+    else:
+        names = [name]
+    parser.add_argument(*names, **spec)
+
+
+def _setup_entrypoint(invoker, description, name, param_specs, func):
+    name = name or func.__name__
+    desc = description or (func.__doc__ and func.__doc__.splitlines()[0])
+    parser = ArgumentParser(prog=name, description=desc)
+    for param_name, param_spec in param_specs.items():
+        _add_argument(parser, param_name, param_spec)
+    func.invoke = partial(_invoke_wrapper, invoker, func, parser)
+    # Make this info accessible later, for generating pyproject.toml content
+    # and for testing purposes.
+    func.entrypoint_name = name
+    func.entrypoint_desc = desc
+    return func
+
+
+def _entrypoint(invoker, *, description=None, name=None, params=None, **kwargs):
+    return partial(
+        _setup_entrypoint, invoker, description, name, params or kwargs
+    )
+
+
+def make_entrypoint(invoker):
+    """Create an entrypoint decorator from an invoker function."""
+    return partial(_entrypoint, invoker)
+
+
+def invoke(func, args):
+    """Default invoker."""
+    return func(**args)
+
+
+# We don't just apply the decorator because we want clients to be able to use
+# the default `invoke` to help implement their own invokers.
+entrypoint = make_entrypoint(invoke)
--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -1,5 +1,79 @@
-from entrypoint import __version__
+from entrypoint import __version__, entrypoint
+import pytest
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@entrypoint(
+    foo='the value for foo',
+    bar='the value for bar',
+    baz='the value for baz'
+)
+def example(foo, bar, baz):
+    """An example entry point for testing.
+
+    The rest of the function __doc__ is ignored by the entrypoint library.
+    foo -> a foo value.
+    bar -> a bar value.
+    baz -> a baz value.
+
+    Returns a string indicating what was passed in."""
+    return f'foo={foo}, bar={bar}, baz={baz}'
+
+
+@entrypoint(
+    description='An example with custom name and description.',
+    name='renamed',
+    foo='the value for foo',
+    bar='the value for bar',
+    baz='the value for baz'
+)
+def to_rename(foo, bar, baz):
+    """This text should be replaced explicitly."""
+    return f'foo={foo}, bar={bar}, baz={baz}'
+
+
+@entrypoint(
+    params={'description': 'description', 'name': 'name'}
+)
+def tricky(description, name):
+    pass
+
+
+def commandline(func, s):
+    return func.invoke(s.split())
+
+
+def test_original_funcs():
+    assert example(1, 2, 3) == 'foo=1, bar=2, baz=3'
+    assert example.entrypoint_name == 'example'
+    assert example.entrypoint_desc == 'An example entry point for testing.'
+    assert to_rename(1, 2, 3) == 'foo=1, bar=2, baz=3'
+    assert to_rename.entrypoint_name == 'renamed'
+    assert to_rename.entrypoint_desc == 'An example with custom name and description.'
+    assert tricky.entrypoint_name == 'tricky'
+    assert tricky.entrypoint_desc is None
+
+
+@pytest.mark.parametrize('func', [example, to_rename])
+def test_good_commandline(func):
+    assert commandline(func, '4 5 6') == 'foo=4, bar=5, baz=6'
+
+
+@pytest.mark.parametrize('func', [example, to_rename])
+@pytest.mark.parametrize('s', ['', '1', '1 2', '1 2 3 4'])
+def test_bad_commandlines(capsys, func, s):
+    with pytest.raises(SystemExit):
+        commandline(func, s)
+    output = capsys.readouterr().err.splitlines()
+    assert output[0].startswith(f'usage: {func.entrypoint_name}')
+    assert output[1].startswith(f'{func.entrypoint_name}: error:')
+
+
+@pytest.mark.parametrize('func', [example, to_rename])
+@pytest.mark.parametrize('s', ['-h', '--help'])
+def test_help_commandlines(capsys, func, s):
+    with pytest.raises(SystemExit):
+        commandline(func, s)
+    output = capsys.readouterr().out.splitlines()
+    assert output[0].startswith(f'usage: {func.entrypoint_name}')
+    assert not output[1] # should be a blank line
+    assert output[2] == func.entrypoint_desc
