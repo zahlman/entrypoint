f13dc668dd7b1afee7a6ac3f5315cfde2e20c02d
Karl Knechtel <karl.a.knechtel@gmail.com>
2020-08-06 02:53:54 -0400

    Move dispatching logic into the Parser ABC and rm corresponding decorator parameter. Instead of expecting custom dispatch logic, a hook is provided to wrap the actual call once dispatch args have been determined.

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -40,14 +40,14 @@ def _add_to_parser(parser, param_name, spec, signature):
 
 
 def _setup_entrypoint(
-    dispatch, parser_class, parser_args, specs, func
+    parser_class, parser_args, specs, func
 ):
     # set defaults if missing or empty
     name = parser_args.get('name', '') or func.__name__
     doc_top = func.__doc__.splitlines()[0] if func.__doc__ else ''
     description = parser_args.get('description', '') or doc_top
     parser_args['name'], parser_args['description'] = name, description
-    parser = parser_class(dispatch, func, parser_args)
+    parser = parser_class(func, parser_args)
     for param_name, spec in specs.items():
         _add_to_parser(parser, param_name, spec, signature_of(func))
     func.invoke = parser.invoke
@@ -59,55 +59,8 @@ def _setup_entrypoint(
     return func
 
 
-def default_dispatch(func, args):
-    """Default invoker."""
-    # Any errors that occur here should be treated as programming errors,
-    # because they indicate that the interface created through the
-    # entrypoint decorator is broken (does not reliably map to the underlying
-    # function's parameters). So we use assertions.
-    positional = []
-    # `args` come from the wrapper function and are not externally accessible,
-    # so there is no need to make a copy even though we delete keys.
-    keywords = args
-    has_kwargs_param = False
-    explicit_keywords = {}
-    for name, param in signature_of(func).parameters.items():
-        assert not has_kwargs_param # just a sanity check.
-        if param.kind == Parameter.VAR_KEYWORD:
-            has_kwargs_param = True # this should be the last parameter.
-            continue
-        if name in keywords:
-            arg = keywords[name]
-            del keywords[name]
-        else:
-            assert param.default != _empty, \
-            f'command-line args missing necessary value for `{name}`'
-            arg = keywords.get(name, param.default)
-        if param.kind == Parameter.VAR_POSITIONAL:
-            try:
-                iter(arg)
-            except TypeError:
-                assert False, \
-                'command-line arg for VAR_POSITIONAL parameter must be iterable'
-            positional.extend(arg)
-        elif param.kind == Parameter.POSITIONAL_OR_KEYWORD:
-            positional.append(arg)
-        elif param.kind == Parameter.KEYWORD_ONLY:
-            explicit_keywords[name] = arg
-        else:
-            # POSITIONAL_ONLY (C interface stuff) is disallowed for now.
-            assert False, \
-            f'`{param.kind!s}` parameter in function signature not allowed'
-    if not has_kwargs_param:
-        k = set(keywords.keys())
-        assert not k, f'extra unusable command-line arguments found: {k}'
-    assert not set(explicit_keywords.keys()) & set(keywords.keys())
-    return func(*positional, **explicit_keywords, **keywords)
-
-
 def entrypoint(
-    *, dispatch=default_dispatch, parser_class=DefaultParser,
-    parser_args=None, specs=None, **kwargs
+    *, parser_class=DefaultParser, parser_args=None, specs=None, **kwargs
 ):
     parser_args = {} if parser_args is None else parser_args.copy()
     specs = {} if specs is None else specs.copy()
@@ -115,5 +68,5 @@ def entrypoint(
     for key, value in kwargs.items():
         (parser_args if key in to_parser else specs)[key] = value
     return partial(
-        _setup_entrypoint, dispatch, parser_class, parser_args, specs
+        _setup_entrypoint, parser_class, parser_args, specs
     )
--- entrypoint/parser.py
+++ entrypoint/parser.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
 from argparse import ArgumentParser
+from inspect import Parameter, signature as signature_of, _empty
+import sys
 
 
 class Parser(ABC):
     """Abstract base class for Parsers.
 
     A Parser implements the core logic for an entrypoint decorator."""
-    def __init__(self, dispatcher:callable, func: callable, config:dict):
+    def __init__(self, func: callable, config:dict):
         """Contructor.
         dispatcher -> maps parsed arguments onto the decorated function.
         func -> the decorated function.
         config -> additional configuration options."""
-        self._dispatcher = dispatcher
         self._func = func
 
 
@@ -35,7 +36,7 @@ class Parser(ABC):
 
     @abstractmethod
     def add_argument(self, name:str, decorator_spec:dict, param_spec:dict):
-        """Specify a command-line option.
+        """Specify a command-line argument.
         name -> name of the intended recipient parameter.
         decorator_spec -> parsing options from the decorator.
         Format and interpretation is up to the concrete parser.
@@ -51,20 +52,85 @@ class Parser(ABC):
         raise NotImplementedError
 
 
+    def _get_dispatched_args(self, parsed_args):
+        """Default invoker."""
+        # Any errors that occur here should be treated as programming errors,
+        # because they indicate that the interface created through the
+        # entrypoint decorator is broken (does not reliably map to the
+        # underlying function's parameters). So we use assertions.
+        positional = []
+        # `parsed_args` were created locally, so we don't need a copy
+        # even though we delete keys.
+        keywords = parsed_args
+        has_kwargs_param = False
+        explicit_keywords = {}
+        for name, param in signature_of(self._func).parameters.items():
+            assert not has_kwargs_param # just a sanity check.
+            if param.kind == Parameter.VAR_KEYWORD:
+                has_kwargs_param = True # this should be the last parameter.
+                continue
+            if name in keywords:
+                arg = keywords[name]
+                del keywords[name]
+            else:
+                assert param.default != _empty, \
+                f'command-line args missing necessary value for `{name}`'
+                arg = keywords.get(name, param.default)
+            if param.kind == Parameter.VAR_POSITIONAL:
+                try:
+                    iter(arg)
+                except TypeError:
+                    assert False, \
+                    'command-line arg for VAR_POSITIONAL parameter must be iterable'
+                positional.extend(arg)
+            elif param.kind == Parameter.POSITIONAL_OR_KEYWORD:
+                positional.append(arg)
+            elif param.kind == Parameter.KEYWORD_ONLY:
+                explicit_keywords[name] = arg
+            else:
+                # POSITIONAL_ONLY (C interface stuff) is disallowed for now.
+                assert False, \
+                f'`{param.kind!s}` parameter in function signature not allowed'
+        if not has_kwargs_param:
+            k = set(keywords.keys())
+            assert not k, f'extra unusable command-line arguments found: {k}'
+        assert not set(explicit_keywords.keys()) & set(keywords.keys())
+        return positional, explicit_keywords, keywords
+
+
+    def call_with(self, positional:list, explicit:dict, keywords:dict):
+        """Default hook for calling the decorated function.
+        Displays the return value (on stdout) or exception message (on stderr)
+        and exits with an appropriate return code.
+        
+        positional -> positional arguments for the call.
+        explicit -> keyword-only arguments for the call.
+        keywords -> `**kwargs` arguments for the call.
+        Modify any of these prior to calling at your own risk.
+        You can access the function to call as `self._func`.
+        """
+        return self._func(*positional, **explicit, **keywords)
+
+
     def invoke(self, command_line=None):
         """Call the decorated function using parsed arguments.
-        This should not be overridden - replace the dispatcher instead.
-        command_line -> list of tokens to parse, or None (use `sys.argv`)."""
-        return self._dispatcher(self._func, self.parse(command_line))
 
+        command_line -> list of tokens to parse, or None (use `sys.argv`).
+        
+        This should not be overridden - `call_with` is the hook you want.
+        This will ordinarily not return; `call_with` should use `sys.exit`
+        to give a return value back to the OS."""
+        return self.call_with(
+            *self._get_dispatched_args(self.parse(command_line))
+        )
 
 
 class DefaultParser(Parser):
     """Default implementation of a Parser.
     
     Delegates to an `argparse.ArgumentParser` to do the work."""
-    def __init__(self, dispatcher:callable, func:callable, config:dict):
-        super().__init__(dispatcher, func, config)
+    def __init__(self, func:callable, config:dict):
+        super().__init__(func, config)
         self._impl = ArgumentParser(
             prog=config.get('name', ''),
             description=config.get('description', '')
