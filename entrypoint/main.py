4233aac2fe7cc5d0572f67213598b43825805bbd
Karl Knechtel <karl.a.knechtel@gmail.com>
2020-08-11 01:49:46 -0400

    Renaming entry points now uses a template, and conversion of underscores to hyphens is default

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -7,23 +7,29 @@ from .dispatcher import DefaultDispatcher
 _REGISTRY = {}
 
 
+def _prepare_name(func, parser_args):
+    name = parser_args.get('name', '{name}').format(
+        name=func.__name__.replace('_', '-'), _name=func.__name__
+    )
+    parser_args['name'] = func.entrypoint_name = name
+    _REGISTRY[name] = f'{func.__module__}:{func.__name__}.invoke'
+
+
+def _prepare_description(func, parser_args):
+    doc_top = func.__doc__.splitlines()[0] if func.__doc__ else ''
+    description = parser_args.get('description', '') or doc_top
+    parser_args['description'] = func.entrypoint_desc = description
+
+
 def _setup_entrypoint(
     dispatch_class, parser_class, parser_args, specs, func
 ):
-    # set defaults if missing or empty
-    name = parser_args.get('name', '') or func.__name__
-    doc_top = func.__doc__.splitlines()[0] if func.__doc__ else ''
-    description = parser_args.get('description', '') or doc_top
-    parser_args['name'], parser_args['description'] = name, description
+    _prepare_name(func, parser_args)
+    _prepare_description(func, parser_args)
     func.invoke = parser_class(
         dispatch_class(signature_of(func).parameters.items()),
         func, parser_args, specs
     ).invoke
-    # Make this info accessible later, for generating pyproject.toml content
-    # and for testing purposes.
-    func.entrypoint_name = name
-    func.entrypoint_desc = description
-    _REGISTRY[name] = f'{func.__module__}:{func.__name__}.invoke'
     return func
 
 
